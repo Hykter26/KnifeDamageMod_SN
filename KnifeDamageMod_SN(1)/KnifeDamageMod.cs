87fb91fbb640d0850973a79e0750e7d59cac4844
Arthur <Arthur@DESKTOP-P690024>
2023-06-15 04:50:26 -0400

    Add project files.

---
+++ KnifeDamageMod_SN(1)/KnifeDamageMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx.Logging;
+using System.CodeDom;
+using static VFXParticlesPool;
+using Hykter26.KnifeDamageModSN;
+
+namespace KnifeDamageMod_SN_1_
+{
+    public static class KnifeDamageMod_SN
+    {
+        [HarmonyPatch(typeof(PlayerTool))]
+        public static class PlayerTool_Patch
+        {
+            [HarmonyPatch(nameof(PlayerTool.Awake))]
+            [HarmonyPostfix]
+            public static void Awake_Postfix(PlayerTool __instance)
+            {
+                // Check to see if this is the knife
+                if (__instance.GetType() == typeof(Knife))
+                {
+
+                    // Get the damage multiplier from the config file.
+                    float damageMultiplier = KnifeDamagePlugin_SN.ConfigKnifeDamageMultiplier.Value;
+
+                    Knife knife = __instance as Knife;
+                    //double the knife damage v
+                    float knifeDamage = knife.damage;
+                    float newKnifeDamage = knifeDamage * 2.0f;
+                    knife.damage = newKnifeDamage;
+
+                    KnifeDamagePlugin_SN.logger.Log(LogLevel.Info, $"Knife damage was: {knifeDamage}," +
+                        $" is now: {newKnifeDamage}");
+
+                }
+            }
+        }
+    }
+}
---
+++ KnifeDamageMod_SN(1)/KnifeDamagePlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Hykter26.KnifeDamageModSN
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class KnifeDamagePlugin_SN : BaseUnityPlugin
+    {
+        private const string myGUID = "Hykter26/𝕸𝖞𝖑𝖔𝖟.𝖘𝖈𝖞𝖙#5037.knifedamagemodsn";
+        private const string pluginName = "Knife Damage Mod SN";
+        private const string versionString = "1.0.0";
+
+        // Enhancing the MOD
+        // Declare damage multiplier config entry
+        public static ConfigEntry<float> ConfigKnifeDamageMultiplier;
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger = new ManualLogSource(pluginName);
+
+        private void Awake()
+        {
+            // Enhancing the MOD
+            // Setup damage multiplier config entry
+            ConfigKnifeDamageMultiplier = Config.Bind("General",        // The section under which the option is shown
+                "KnifeDamageMultiplier",                                // The key of the configuration option
+                1.0f,                                                   // The default value
+                "Knife damage multiplier.");   							// Description of the config value
+
+            // Patch in our MOD
+            harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {pluginName}, VersionString: {versionString} is loaded.");
+            logger = Logger;
+        }
+    }
+}
